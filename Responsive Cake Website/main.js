a992c1fab859149097814b7033fc0603f6a0a2b5
Chandan Kumar <128698211+kmrchandan006@users.noreply.github.com>
2024-06-04 01:24:12 +0530

    Cake website

---
+++ Responsive Cake Website/main.js
@@ -0,0 +1,24 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
