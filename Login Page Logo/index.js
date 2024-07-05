615aca395756f91d38427d5f298fbc563a1edc35
Chandan Kumar <128698211+kmrchandan006@users.noreply.github.com>
2024-07-05 09:03:37 +0530

    LOgin Page

---
+++ Login Page Logo/index.js
@@ -0,0 +1,22 @@
+$(function() {
+    alert("Please Upvote if you like the code.")
+    $("button").click(function(){
+       var a = $("#text").value();
+       var b = $("#pass").value();
+       if (a == "Saviee" && b == "saviee") {
+           alert("Login Complete")
+       }
+       else{
+           alert("Incorrect Username or Password")
+       }
+    });
+});
+function a() {
+    alert("Awwwwnnn, So sad!")
+}
+function b() {
+    alert("Awwwwnnn, You are so sweet!")
+}
+function c() {
+    alert("Common, we both know its not gonna work!")
+}
